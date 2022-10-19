2e8ec4a3868c44c7cdea8b44345f05b5a00959a9
Jeff Helman <jeffhelman42@gmail.com>
2022-10-19 15:54:37 -0700

    refactor function defs

--- lib/product_filters.ex
+++ lib/product_filters.ex
@@ -57,50 +57,47 @@ defmodule ProductFilters do
     iex> ProductFilters.filter([%{name: "Laptop", category: :tech, price: 100}], min: 50, max: 200, name: "Laptop", category: :tech)
     """
     # TO DO: Refactor this to NinjaLand
-    def filter(products, filters \\ nil) # Idiomatic way of declaring default value
+    def filter(products, filters \\ []) # Idiomatic way of declaring default value
     # The "more correct" default (above) would be an empty list [].
 
     # Deal with the JON doctrine (no filters):
-    def filter(products, filters) when is_nil(filters), do: products
+    def filter(products, []) do
+      products
+    end
 
-    def filter(products, [{key, value}]) when key == :category do
+    # Refactor: use pattern-matching instead of guard:
+    def filter(products, {:category, value}) do
       Enum.filter(products, fn item ->
         item.category == value
       end)
     end
 
-    def filter(products, [{key1, value}, {key2, value}]) do
-      if Keyword.has_key?([{key1, value}, {key2, value}], :min)
-        and Keyword.has_key?([{key1, value}, {key2, value}], :max) do
-        Enum.filter(products, fn item ->
-          item.price >= Keyword.get([{key1, value}, {key2, value}], :min)
-            and item.price <= Keyword.get([{key1, value}, {key2, value}], :max)
-        end)
-      end
-    end
-
-    def filter(products, [{key, value}]) when key == :min do
+    # Refactor: use pattern-matching instead of guard:
+    def filter(products, {:min, value}) do
       Enum.filter(products, fn item ->
         item.price >= value
       end)
     end
 
-    def filter(products, [{key, value}]) when key == :max do
+    # Refactor: use pattern-matching instead of guard:
+    def filter(products, {:max, value}) do
       Enum.filter(products, fn item ->
         item.price <= value
       end)
     end
 
-    def filter(products, [{_key, value}]) do
+    def filter(products, {:name, value}) do
       Enum.filter(products, fn item ->
         String.contains?(String.upcase(item.name), String.upcase(value))
       end)
     end
 
+    # Ninja refactoring via Icia's brilliance, Jon's typing, and Jeff's awe:
+    # Built to pass the "min and max" test, but this turns out to drive ALL the tests
     def filter(products, [h_filter | t_filters]) do
-      IO.inspect(h_filter)
-      IO.inspect(t_filters)
+      IO.inspect(h_filter, label: "h_filter")
+      IO.inspect(t_filters, label: "t_filters")
       filter(products, h_filter) |> filter(t_filters)
     end
-    
+
   end # ... of OUR MODULE, George!!
--- test/product_filters_test.exs
+++ test/product_filters_test.exs
@@ -12,10 +12,10 @@ defmodule ProductFiltersTest do
   test "filter/2 empty filters" do
     # INPUT product list & no filters
     # OUTPUT the same product list
-    assert ProductFilters.filter(@products, nil) == @products
+    assert ProductFilters.filter(@products, []) == @products
   end
 
-  @tag :skip
+  # @tag :skip
   test "filter/2 by exact matching name" do
     assert ProductFilters.filter(@products, name: "Laptop") ==
              [%{name: "Laptop", category: :tech, price: 100_000}]
@@ -24,7 +24,7 @@ defmodule ProductFiltersTest do
              [%{name: "Phone", category: :tech, price: 50000}]
   end
 
-  @tag :skip
+  # @tag :skip
   test "filter/2 by partial matching name" do
     assert ProductFilters.filter(@products, name: "apt") ==
              [%{name: "Laptop", category: :tech, price: 100_000}]
@@ -36,7 +36,7 @@ defmodule ProductFiltersTest do
              ]
   end
 
-  @tag :skip
+  # @tag :skip
   test "filter/2 by mixed case partial matching name" do
     assert ProductFilters.filter(@products, name: "L") ==
              [
@@ -83,7 +83,7 @@ defmodule ProductFiltersTest do
            ]
   end
 
-  @tag :skip
+  # @tag :skip
   test "filter/2 by max price" do
     assert ProductFilters.filter(@products, max: 50000) == [
              %{name: "Phone", category: :tech, price: 50000},
@@ -94,15 +94,17 @@ defmodule ProductFiltersTest do
     assert ProductFilters.filter(@products, max: 1000) == [
              %{name: "Chocolate", category: :snacks, price: 200},
              %{name: "Shampoo", category: :health, price: 1000}
-           ] 
+           ]
   end
 
-  @tag :skip
+  # @tag :skip
   test "filter/2 by max and min price" do
-    assert ProductFilters.filter(@products, min: 1999, max: 1000) == [
+    assert Enum.sort(ProductFilters.filter(@products, min: 199, max: 51000), :asc) ==
+      Enum.sort([
       %{name: "Chocolate", category: :snacks, price: 200},
+      %{name: "Phone", category: :tech, price: 50000},
       %{name: "Shampoo", category: :health, price: 1000}
-    ]
+    ], :asc)
 
   end
 
